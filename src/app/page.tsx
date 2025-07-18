6f4cee6b47184efb5c07668b58b25634fd88d5b8
Andres-XL <amovillaw01@gmail.com>
2025-07-17 19:28:56 -0500

    clean up some files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import XMLFormatter from "~/_components/xml-formatter";
-import LocaleSwitcher from "~/_components/locale-switcher";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <main className="h-screen max-h-screen bg-white pb-4 pt-16 transition-colors dark:bg-gray-900">
-      <LocaleSwitcher />
-      <XMLFormatter />
-    </main>
-  );
+export default function RootPage() {
+  redirect("/en");
 }
